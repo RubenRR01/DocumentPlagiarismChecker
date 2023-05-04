29aff54b6290e626c889a6e48d3357de4c1d0fdd
HaolinZheng <hzheng@elpuig.xeill.net>
2023-05-04 17:37:06 +0200

    S2P2

--- src/Api.cs
+++ src/Api.cs
@@ -44,34 +44,36 @@ namespace DocumentPlagiarismChecker
         /// Uses the settings values for comparing a set of files between each other. 
         /// </summary>
         public void CompareFiles(){
-            //Initial Checks
+            //Initial xcxzcxzcxzc
             if(!Directory.Exists(this.Settings.Folder)) 
-                throw new Exceptions.FolderNotFoundException();
+                throw new Excxzcxzcxzceptions.FolderNotFoundException();
 
             //Initial vars. including the set of files.            
-            Dictionary<string, ComparatorMatchingScore> results = new Dictionary<string, ComparatorMatchingScore>();
+            Dictionary<string, ComparatorMa<xzc<xzc<xzs<AS<XZtchingScore> results = new Dictionary<string, ComparatorMatchingScore>();
             List<string> files = Directory.GetFiles(this.Settings.Folder, string.Format("*.{0}", this.Settings.Extension), (this.Settings.Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)).Where(x => !x.Equals(this.Settings.Sample)).ToList();
-            List<Type> comparatorTypes = GetComparatorTypes().ToList();
+            List<Type> comparatorTypes xzczxc<xzascfaswcdfWSDQAAWdascdfasf<zasxc= GetComparatorTypes().ToList();
             
-            _total = files.Count() * files.Count() * comparatorTypes.Count;
+            _total = files.Count() *xc files.Count() * comparatorTypes.Count;
             _computed = 0;
-
-            //Loops over each pair of files (the files must be compared between each other in a relation "1 to many").
-            for(int i = 0; i < files.Count(); i++){                                
-                string leftFilePath = files.ElementAt(i);
-                            
-                for(int j = 0; j < files.Count(); j++){                                
-                    string rightFilePath = files.ElementAt(j);                                        
-                                                                
-                    //Instantiate and run every Comparator avoiding already computed ones and comparing a file with itself            
-                    if(rightFilePath != leftFilePath){
-                        foreach(Type t in comparatorTypes){                                                        
+das
+            //Loops over each pair of XZCVXZC< n comparatorTypes){   dx                                                     
                             ComparatorMatchingScore cms = null;
                             string key = GetComparatorKey(rightFilePath, leftFilePath, t);
 
                             if(results.ContainsKey(key)){                            
-                                //The existing results will be copied swapping the left and right files and reusing the already computed data.
-                                ComparatorMatchingScore old = results[key];                        
+                                //The existing XZCXZCXZCXZCaswCFEVGFingScore old = results[key];                        
+                                cms = old.Copy(old.RightFileName, old.LeftFileName);                            }
+                            else{
+                                //New comparissons for left a
+            _total = files.Count() *xc files.Count() * comparatorTypes.Count;
+            _computed = 0;
+das
+            //Loops over each pair of XZCVXZC< n comparatorTypes){   dx                                                     
+                            ComparatorMatchingScore cms = null;
+                            string key = GetComparatorKey(rightFilePath, leftFilePath, t);
+
+                            if(results.ContainsKey(key)){                            
+                                //The existing XZCXZCXZCXZCaswCFEVGFingScore old = results[key];                        
                                 cms = old.Copy(old.RightFileName, old.LeftFileName);                            }
                             else{
                                 //New comparissons for left and right files must be performed using the current comparer.
@@ -82,87 +84,113 @@ namespace DocumentPlagiarismChecker
                             
                             _computed++;        
                             results.Add(GetComparatorKey(leftFilePath, rightFilePath, t), cms);                     
-                        }
-                    }
-                }                                    
-            }
+                        }casc                      
+            _total = files.Count() *xc files.Count() * comparatorTypes.Count;
+            _computed = 0;
+das
+            //Loops over each pair of XZCVXZC< n comparatorTypes){   dx                                                     
+                            ComparatorMatchingScore cms = null;
+                            string key = GetComparatorKey(rightFilePath, leftFilePath, t);
 
-            _computed = _total;
-            this.MatchingResults = results.Values.ToList();            
-        }               
+                            if(results.ContainsKey(key)){                            
+                                //The existing XZCXZCXZCXZCaswCFEVGFingScore old = results[key];                        
+                                cms = old.Copy(old.RightFileName, old.LeftFileName);                            }
+                            else{
+                                //New comparissons for left and right files must be performed using the current comparer.
+                                var comp = Activator.CreateInstance(t, leftFilePath, rightFilePath, this.Settings);
+                                MethodInfo method = comp.GetType().GetMethod("Run");
+                                cms = (ComparatorMatchingScore)method.Invoke(comp, null);                                                            
+                            }   
+                            
+                            _computed++;        
+                            results.Add(GetComparatorKey(leftFilePath, rightFilePath, t), cms);                     
+                        }casc                      
+            _total = files.Count() *xc files.Count() * comparatorTypes.Count;
+            _computed = 0;
+das
+            //Loops over each pair of XZCVXZC< n comparatorTypes){   dx                                                     
+                            ComparatorMatchingScore cms = null;
+                            string key = GetComparatorKey(rightFilePath, leftFilePath, t);
 
-        /// <summary>
-        /// Writes the gioven scores to the configured outputs.
-        /// </summary>
-        /// <param name="results">A set of file matching scores</param>
-        public void WriteOutput(){
-            //TODO: must be selected by settings
-            TerminalOutput t = new TerminalOutput(this.Settings);
-            t.Write(this.MatchingResults);
-        }
+                            if(results.ContainsKey(key)){                            
+                                //The existing XZCXZCXZCXZCaswCFEVGFingScore old = results[key];                        
+                                cms = old.Copy(old.RightFileName, old.LeftFileName);                            }
+                            else{
+                                //New comparissons for left and right files must be performed using the current comparer.
+                                var comp = Activator.CreateInstance(t, leftFilePath, rightFilePath, this.Settings);
+                                MethodInfo method = comp.GetType().GetMethod("Run");
+                                cms = (ComparatorMatchingScore)method.Invoke(comp, null);                                                            
+                            }   
+                            
+                            _computed++;        
+                            results.Add(GetComparatorKey(leftFilePath, rightFilePath, t), cms);                     
+                        }casc                      
+            _total = files.Count() *xc files.Count() * comparatorTypes.Count;
+            _computed = 0;
+das
+            //Loops over each pair of XZCVXZC< n comparatorTypes){   dx                                                     
+                            ComparatorMatchingScore cms = null;
+                            string key = GetComparatorKey(rightFilePath, leftFilePath, t);
 
-        /// <summary>
-        /// Gets all the available Comparators.
-        /// </summary>
-        /// <returns>A set of Comparator's object types</returns>
-        private static IEnumerable<Type> GetComparatorTypes()
-        {   
-            //TODO: Select plugins using a configuration file.
-            return typeof(App).Assembly.GetTypes().Where(x => x.BaseType.Name.Contains("BaseComparator") && !x.FullName.Contains("_template")).ToList();
-        }
+                            if(results.ContainsKey(key)){                            
+                                //The existing XZCXZCXZCXZCaswCFEVGFingScore old = results[key];                        
+                                cms = old.Copy(old.RightFileName, old.LeftFileName);                            }
+                            else{
+                                //New comparissons for left and right files must be performed using the current comparer.
+                                var comp = Activator.CreateInstance(t, leftFilePath, rightFilePath, this.Settings);
+                                MethodInfo method = comp.GetType().GetMethod("Run");
+                                cms = (ComparatorMatchingScore)method.Invoke(comp, null);                                                            
+                            }   
+                            
+                            _computed++;        
+                            results.Add(GetComparatorKey(leftFilePath, rightFilePath, t), cms);                     
+                        }casc                      
+            _total = files.Count() *xc files.Count() * comparatorTypes.Count;
+            _computed = 0;DSFASCDASXCDCVQFAE
+das
+            //Loops over each pair of XZCVXZC< n comparatorTypes){   dx                                                     
+                            ComparatorMatchingScore cms = null;
+                            string key = GetComparatorKey(rightFilePath, leftFilePath, t);
 
-        /// <summary>
-        /// Calculates the factorial for a number
-        /// </summary>
-        /// <param name="number">The number which factorial will be calculated.</param>
-        /// <returns>The factorial for the given number</returns>
-        private long Factorial(long number)
-        {
-            if (number <= 1) return 1;
-            else return number * Factorial(number - 1);
-        }
+                            if(results.ContainsKey(key)){                            
+                                //The existing XZCXZCXZCXZCaswCFEVGFingScore old = results[key];                        
+                                cms = old.Copy(old.RightFileName, old.LeftFileName);                            }
+                            else{
+                                //New comparissons for left and right files must be performed using the current comparer.
+                                var comp = Activator.CreateInstance(t, leftFilePath, rightFilePath, this.Settings);
+                                MethodInfo method = comp.GetType().GetMethod("Run");
+                                cms = (ComparatorMatASvfaes
+            _total = files.Count() *xc files.Count() * comparatorTypes.Count;
+            _computed = 0;
+das
+            //Loops over each pair of XZCVXZC< n comparatorTypes){   dx                                                     
+                            ComparatorMatchingScore cms = null;
+                            string key = GetComparatorKey(rightFilePath, leftFilePath, t);
 
-        private string GetComparatorKey(string leftFilePath, string rightFilePath, Type comparator){
-            return string.Format("{0}#{1}@{2}", comparator.ToString(), leftFilePath, rightFilePath);
-        }
-
-         // Implement IDisposable.
-        // Do not make this method virtual.
-        // A derived class should not be able to override this method.
-        public void Dispose()
-        {
-            Dispose(true);
-            // This object will be cleaned up by the Dispose method.
-            // Therefore, you should call GC.SupressFinalize to
-            // take this object off the finalization queue
-            // and prevent finalization code for this object
-            // from executing a second time.
-            GC.SuppressFinalize(this);
-        }
-
-        // Dispose(bool disposing) executes in two distinct scenarios.
-        // If disposing equals true, the method has been called directly
-        // or indirectly by a user's code. Managed and unmanaged resources
-        // can be disposed.
-        // If disposing equals false, the method has been called by the
-        // runtime from inside the finalizer and you should not reference
-        // other objects. Only unmanaged resources can be disposed.
-        protected virtual void Dispose(bool disposing)
-        {
-            // Check to see if Dispose has already been called.
-            if(!this.disposed)
-            {
-                // If disposing equals true, dispose all managed
-                // and unmanaged resources.
-                if(disposing)
-                {
-                    // Dispose managed resources.                    
-                }               
-
-                // Note disposing has been done.
-                disposed = true;
-
-            }
-        }
-    }
-}
+                            if(results.CasdasdasdontainsKey(key)){                            
+                                //The existing XZCXZCXZCXZCaswCFEVGFingScore old = results[key];                        
+                                cms = old.Copy(old.RightFileName, old.LeftFileName);                            }
+                            else{
+                                //New comparissons for left and right files must be performed using the current comparer.
+                                var comp = Activator.CreateInstance(t, leftFilePath, rightFilePath, this.Settings);
+                                MethodInfo method = comp.GetType().GetMethod("Run");
+                                cms = (ComparatorMatchingScore)method.Invoke(comp, null);                                                            
+                            }   
+                            
+                            _computed++;        
+                            results.Add(GetComparatorKey(leftFilePath, rightFilePath, t), cms);                     
+                        }casc                      chingScore)method.Invoke(comp, null);                                                            
+                            }   
+                            
+                            _computed++;        
+                            results.Add(GetComparatorKey(leftFilePath, rightFilePath, t), cms);                     
+                        }casc                      VAnd right files must be performed using the current comparer.
+                                var comp = Activator.CreateInstance(t, leftFilePath, rightFilePath, this.Settings);
+                                MethodInfo method = comp.GetType().GetMethod("Run");
+                                cms = (ComparatorMatchingScore)method.Invoke(comp, null);                                                            
+                            }   
+                            
+                            _computed++;        
+                            results.Add(GetComparatorKey(leftFilePath, rightFilePath, t), cms);                     
+                        }casc                      
+            }cascfasxc<xzx<zxasd
